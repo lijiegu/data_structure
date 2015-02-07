19f11d8d726799791e9a2e50f37982d67f7967a9
lhyqie <lhymvp@gmail.com>
2015-02-07 02:26:24 -0600

    add a task for TestArray

--- src/array/TestArray.java
+++ src/array/TestArray.java
@@ -1,5 +1,12 @@
 package array;
 
 public class TestArray {
-
+	public static void main(String[] args) {
+		int a [] = {1,2,3};
+		int sum = 0;
+		for (int i : a) {
+			sum += i;
+		}
+		System.out.println("sum = "+ sum);
+	}
 }
