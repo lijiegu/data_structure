cbeae7c0e7b19141fbb0e272d1fb6980d44128de
lijiegu <593998524@qq.com>
2015-02-07 16:17:46 +0800

    First commit

---
+++ src/data_structure/HelloWorld.java
@@ -0,0 +1,12 @@
+package data_structure;
+
+public class HelloWorld {
+
+	public static void print1(){
+		System.out.println("print1()");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		print1();
+	}
+}
