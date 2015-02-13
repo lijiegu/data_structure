8b4b6580b93df4d1bc9505e4a984cd26c034bb3f
lijiegu <593998524@qq.com>
2015-02-13 22:08:30 +0800

    /*2.1 To the HighArray class in the highArray.java program (Listing 2.3), add a method called getMax() that returns the value of the highest key in the array, or –1 if the array is empty. Add some code in main() to exercise this method. You can assume all the keys are positive numbers.
    2.2 Modify the method in Programming Project 2.1 so that the item with
    the
    highest key is not only returned by the method, but also removed from
    the
    array. Call the method removeMax().*/
    
    //问题在9行  有注释的

---
+++ src/HighArray/HighArray.java
@@ -0,0 +1,62 @@
+package HighArray;
+
+public class HighArray {
+	private long[] array;
+	private int nElems;
+
+	public HighArray(int maxSize){
+		array=new long[maxSize];
+		nElems=0;
+	}
+	
+	public void insert(long value){
+		array[nElems]=value;
+		nElems++;
+	}
+	
+	public boolean search(long key){
+		int i;							
+		for(i=0; i<nElems; i++){
+			if(array[i]==key)	break;
+		}
+		if(i==nElems)	return false;//为了让此处的i为已知量，int i;得在for外面完成。
+		else 			return true;
+	}
+	
+	public void delete(long key){
+		int i;
+		for(i=0; i<nElems; i++){
+			if(array[i]==key)	break;
+		}
+		if(i!=nElems)
+			for(int j=i; j<nElems; j++){
+				array[j]=array[j+1];
+			}
+		nElems--;
+		
+	}
+	
+	public long getMax(){
+		if(array==null)	return -1;
+		else{
+			long max=array[0];
+			for(int i=1; i<nElems; i++){
+				if(array[i]>max)	max=array[i];
+			}
+		return max;
+		}
+	}
+	
+	public void removeMax(){
+		long rMax=getMax();
+		if(rMax==-1)	System.out.println("空数组！");
+		else	delete(rMax);
+	}
+	
+	public void display(){
+		for(int i=0; i<nElems; i++){
+			System.out.print(array[i]+" ");
+		}
+		System.out.println("");
+	}
+}
